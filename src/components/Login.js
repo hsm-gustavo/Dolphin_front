74afb5a546b633b2a56872c53515bb35691b1c1b
hsm-gustavo <gustavo.hs.malaquias@gmail.com>
2024-01-21 02:56:05 -0300

    feat: adição de responsividade

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 
 const Login = () => {
   return (
-    <div className="flex flex-col justify-center items-center w-6/12 h-5/6 px-5 rounded-3xl text-center bg-white">
+    <div className="flex flex-col justify-center items-center md:w-6/12 w-full sm:h-5/6 h-full px-5 md:rounded-3xl text-center bg-white shadow-2xl">
       <img src={logo} alt="logo" className="w-20" />
       <div className="flex flex-col justify-center items-center my-10">
         <h1 className="text-3xl font-bold">Login</h1>
@@ -23,14 +23,8 @@ const Login = () => {
           required
         />
 
-        <label
-          className="flex flex-row justify-between font-semibold text-left mb-2"
-          htmlFor="password"
-        >
+        <label className="font-semibold text-left mb-2" htmlFor="password">
           Password
-          <Link to={123} className="font-medium underline">
-            Forgot password?
-          </Link>
         </label>
 
         <input
@@ -40,13 +34,19 @@ const Login = () => {
           className="border-2 border-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-400 rounded-lg px-3 py-1 pr-20 my-2"
           required
         />
+        <Link to={123} className="font-medium underline">
+          Forgot password?
+        </Link>
         <input
           type="submit"
           value="Login"
           className="my-10 bg-black text-white py-2 rounded-lg hover:bg-slate-900"
         />
         <p>
-          Don't have an account? <Link to={123} className="underline">Sign up</Link>
+          Don't have an account?{" "}
+          <Link to={123} className="underline">
+            Sign up
+          </Link>
         </p>
       </form>
     </div>
