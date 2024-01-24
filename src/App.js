c7d8703740a53ab2cfe1a40598135f39fac27c28
hsm-gustavo <gustavo.hs.malaquias@gmail.com>
2024-01-24 00:10:53 -0300

    feat: adição da localização para inglês da página de login

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import "./App.css"
-import Login from "./components/Login"
+import Login from "./pages/Login"
+import Navbar from "./components/Navbar"
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
 function App() {
   return (
-    <div className="App flex flex-col justify-center items-center bg-blue-900">
+    <div className="flex flex-col justify-center items-center bg-blue-900 h-screen">
+      {/* TODO -> see react-router-dom v6.x */}
       <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Login />} />
         </Routes>
