3a925eec5d71386e6c9c5acb0470e02974f3e691
hsm-gustavo <gustavo.hs.malaquias@gmail.com>
2024-02-06 00:49:05 -0300

    fix: removed unnecessary imports and comments, changed from username to email, reusing input component

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,16 +1,12 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import logo from "../assets/logo.png";
+import logo from "../assets/logo.svg";
 import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
+import Input from "../components/Input";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
   const { t } = useTranslation();
-  /* const MySwal = withReactContent(Swal); */
 
   const getLogin = async ({ username, password }) => {
     return await fetch("http://localhost:3001/login", {
@@ -28,6 +24,8 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const username = e.target.username.value;
+    const password = e.target.password.value;
     const response = await getLogin({ username, password });
     if ("user" in response) {
       Swal.fire({
@@ -52,44 +50,61 @@ const Login = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-blue-900">
       <div className="flex h-full w-full flex-col items-center justify-center bg-white px-5 text-center shadow-2xl sm:h-5/6 md:w-6/12 md:rounded-3xl">
-        <img src={logo} alt="logo" className="w-20" />
+        <img src={logo} alt="logo" className="w-28 " />
         <div className="my-10 flex flex-col items-center justify-center">
-          <h1 className="text-3xl font-bold">Login</h1>
+          <h1 className="text-3xl font-bold">{t("login.login")}</h1>
           <p className="text-lg text-slate-500">{t("login.msg")}</p>
         </div>
-        <form className="flex flex-col">
-          <label className="text-left font-semibold" htmlFor="username">
-            {t("login.username")}
-          </label>
-          <input
-            type="text"
-            name="username"
-            placeholder={t("login.username")}
-            className="my-2 rounded-lg border-2 border-slate-100 px-3 py-1 pr-20 focus:outline-none focus:ring-2 focus:ring-slate-400"
-            required
-            onChange={(e) => setUsername(e.target.value)}
+        <form className="flex flex-col" onSubmit={(e) => handleSubmit(e)}>
+          <Input
+            classNameLabel={["text-left", "font-semibold"]}
+            htmlFor={"username"}
+            label={t("login.email")}
+            type={"email"}
+            name={"username"}
+            placeholder={t("login.email")}
+            required={true}
+            className={[
+              "my-2",
+              "rounded-lg",
+              "border-2",
+              "border-slate-100",
+              "px-3",
+              "py-1",
+              "pr-20",
+              "focus:outline-none",
+              "focus:ring-2",
+              "focus:ring-slate-400",
+            ]}
           />
-
-          <label className="mb-2 text-left font-semibold" htmlFor="password">
-            {t("login.password")}
-          </label>
-
-          <input
-            type="password"
-            name="password"
+          <Input
+            classNameLabel={["mb-2", "text-left", "font-semibold"]}
+            htmlFor={"password"}
+            label={t("login.password")}
+            type={"password"}
+            name={"password"}
             placeholder={t("login.password")}
-            className="my-2 rounded-lg border-2 border-slate-100 px-3 py-1 pr-20 focus:outline-none focus:ring-2 focus:ring-slate-400"
-            required
-            onChange={(e) => setPassword(e.target.value)}
+            required={true}
+            className={[
+              "my-2",
+              "rounded-lg",
+              "border-2",
+              "border-slate-100",
+              "px-3",
+              "py-1",
+              "pr-20",
+              "focus:outline-none",
+              "focus:ring-2",
+              "focus:ring-slate-400",
+            ]}
           />
           <Link to={123} className="font-medium underline">
             {t("login.forgot")}
           </Link>
           <input
             type="submit"
-            value="Login"
+            value={t("login.login")}
             className="my-10 rounded-lg bg-black py-2 text-white hover:bg-slate-900"
-            onClick={(e) => handleSubmit(e)}
           />
           <p>
             {t("login.acc")}{" "}
