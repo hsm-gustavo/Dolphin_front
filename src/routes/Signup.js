912ef0a8f4ef30c5fa5475c3522388024d377fda
hsm-gustavo <gustavo.hs.malaquias@gmail.com>
2024-02-06 00:51:24 -0300

    feat: added input validation, added localization, now using input component

--- src/routes/Signup.js
+++ src/routes/Signup.js
@@ -1,68 +1,225 @@
+import Input from "../components/Input";
+import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
+
 const Signup = () => {
-  /* TODO -> add validation */
+  const { t } = useTranslation();
+
+  const [emailValidation, setEmailValidation] = useState({
+    isValid: true,
+    message: "",
+  });
+  const [passwordValidation, setPasswordValidation] = useState({
+    isValid: true,
+    message: "",
+  });
+
+  const validatePassword = (password, confirmPassword) => {
+    if (password !== confirmPassword) {
+      return { isValid: false, message: "Passwords do not match" };
+    }
+
+    if (password.length < 8) {
+      return {
+        isValid: false,
+        message: "Password must be at least 8 characters",
+      };
+    }
+
+    const hasSymbol = /[!@#$%&*?:<>|]/.test(password);
+    const hasNumber = /\d/.test(password);
+    if (!hasSymbol) {
+      return {
+        isValid: false,
+        message:
+          "Password must contain a symbol (!, @, #, $, %, &, *, ?) and a number (0-9)",
+      };
+    }
+
+    if (!hasNumber) {
+      return {
+        isValid: false,
+        message:
+          "Password must contain a symbol (!, @, #, $, %, &, *, ?) and a number (0-9)",
+      };
+    }
+
+    return { isValid: true, message: "Password is valid" };
+  };
+
+  const validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    if (!re.test(email)) {
+      return { isValid: false, message: "Invalid email" };
+    }
+    return { isValid: true, message: "Email is valid" };
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const firstName = e.target.firstName.value;
+    const lastName = e.target.lastName.value;
+
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const confirmPassword = e.target.confirmPassword.value;
+
+    const passwordValidation = validatePassword(password, confirmPassword);
+
+    if (!passwordValidation.isValid) {
+      setPasswordValidation(passwordValidation);
+      return;
+    }
+
+    const emailValidation = validateEmail(email);
+    if (!emailValidation) {
+      setEmailValidation(emailValidation);
+      return;
+    }
+
+    setEmailValidation({ isValid: true, message: "Email is valid" });
+    setPasswordValidation({ isValid: true, message: "Password is valid" });
+  };
+
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-blue-900">
       <div className="flex h-full w-full flex-col items-center justify-center bg-white px-5 text-center shadow-2xl sm:h-5/6 md:w-6/12 md:rounded-3xl">
+        <img src={logo} alt="logo" className="w-28 " />
         <div className="my-10 flex flex-col items-center justify-center">
-          <h1 className="text-3xl font-bold">Signup</h1>
+          <h1 className="text-3xl font-bold">{t("signup.signup")}</h1>
           <p className="text-lg text-slate-500">
-            Enter your details to create an account
+            {t("signup.msg")}
           </p>
         </div>
-        <form className="flex flex-col">
-          <div className="grid grid-cols-2 auto-rows-auto gap-x-8">
-            <label className="text-left font-semibold col-start-1" htmlFor="firstName">
-              First Name
-            </label>
-            <input
+        <form className="mx-5 flex flex-col" onSubmit={(e) => handleSubmit(e)}>
+          <div className="grid auto-rows-auto grid-cols-2 gap-x-4">
+            <Input
+              htmlFor={"firstName"}
+              classNameLabel={["col-start-1", "text-left", "font-semibold"]}
+              label={t("signup.firstName")}
               type="text"
               name="firstName"
-              placeholder="First Name"
-              className="my-2 rounded-lg border-2 border-slate-100 px-3 py-1 focus:outline-none focus:ring-2 focus:ring-slate-400 col-start-1 row-start-2"
-              required
+              placeholder={t("signup.firstName")}
+              className={[
+                "col-start-1",
+                "row-start-2",
+                "my-2",
+                "rounded-lg",
+                "border-2",
+                "border-slate-100",
+                "px-3",
+                "py-1",
+                "focus:outline-none",
+                "focus:ring-2",
+                "focus:ring-slate-400",
+              ]}
+              required={true}
             />
-            <label className="text-left font-semibold col-start-2" htmlFor="lastName">
-              Last Name
-            </label>
-            <input
+            <Input
+              htmlFor={"lastName"}
+              classNameLabel={["col-start-2", "text-left", "font-semibold"]}
+              label={t("signup.lastName")}
               type="text"
               name="lastName"
-              placeholder="Last Name"
-              className="my-2 rounded-lg border-2 border-slate-100 px-3 py-1 focus:outline-none focus:ring-2 focus:ring-slate-400 col-start-2 row-start-2"
-              required
+              placeholder={t("signup.lastName")}
+              required={true}
+              className={[
+                "col-start-2",
+                "row-start-2",
+                "my-2",
+                "rounded-lg",
+                "border-2",
+                "border-slate-100",
+                "px-3",
+                "py-1",
+                "focus:outline-none",
+                "focus:ring-2",
+                "focus:ring-slate-400",
+              ]}
             />
           </div>
-          <label className="text-left font-semibold" htmlFor="email">
-            Email
-          </label>
-          <input
+          <Input
+            htmlFor={"email"}
+            classNameLabel={["text-left", "font-semibold"]}
+            label={"Email"}
             type="email"
             name="email"
             placeholder="Email"
-            className="my-2 rounded-lg border-2 border-slate-100 px-3 py-1 focus:outline-none focus:ring-2 focus:ring-slate-400"
-            required
+            required={true}
+            className={[
+              "my-2",
+              "rounded-lg",
+              "border-2",
+              "border-slate-100",
+              "px-3",
+              "py-1",
+              "focus:outline-none",
+              "focus:ring-2",
+              "focus:ring-slate-400",
+            ]}
+            validation={emailValidation}
           />
-          <label className="text-left font-semibold" htmlFor="password">
-            Password
-          </label>
+          <div className="grid auto-rows-auto grid-cols-2 gap-x-4">
+            <Input
+              htmlFor={"password"}
+              classNameLabel={["col-start-1", "text-left", "font-semibold"]}
+              label={t("signup.password")}
+              type="password"
+              name="password"
+              placeholder={t("signup.password")}
+              required={true}
+              className={[
+                "col-start-1",
+                "row-start-2",
+                "my-2",
+                "rounded-lg",
+                "border-2",
+                "border-slate-100",
+                "px-3",
+                "py-1",
+                "focus:outline-none",
+                "focus:ring-2",
+                "focus:ring-slate-400",
+              ]}
+              validation={passwordValidation}
+            />
+            <Input
+              htmlFor={"confirmPassword"}
+              classNameLabel={["col-start-2", "text-left", "font-semibold"]}
+              label={t("signup.confirm")}
+              type="password"
+              name="confirmPassword"
+              placeholder={t("signup.confirm")}
+              required={true}
+              className={[
+                "col-start-2",
+                "row-start-2",
+                "my-2",
+                "rounded-lg",
+                "border-2",
+                "border-slate-100",
+                "px-3",
+                "py-1",
+                "focus:outline-none",
+                "focus:ring-2",
+                "focus:ring-slate-400",
+              ]}
+            />
+          </div>
           <input
-            type="password"
-            name="password"
-            placeholder="Password"
-            className="my-2 rounded-lg border-2 border-slate-100 px-3 py-1 focus:outline-none focus:ring-2 focus:ring-slate-400"
-            required
-          />
-          <label className="text-left font-semibold" htmlFor="confirmPassword">
-            Confirm Password
-          </label>
-          <input
-            type="password"
-            name="confirmPassword"
-            placeholder="Confirm Password"
-            className="my-2 rounded-lg border-2 border-slate-100 px-3 py-1 focus:outline-none focus:ring-2 focus:ring-slate-400"
-            required
+            type="submit"
+            value={t("signup.signup")}
+            className="my-10 rounded-lg bg-black py-2 text-white hover:bg-slate-900"
           />
         </form>
+        <p>
+          {t("signup.acc")}{" "}
+          <Link to="/login" className="underline">
+            {t("signup.login")}
+          </Link>
+        </p>
       </div>
     </div>
   );
